cff8c1d38922012d71fb3a6da24288e88a0d0e54
NARENDRA JINKA <NARENDRA JINKA@LAPTOP-M73EGCUP>
2023-12-05 16:09:11 +0530

    hjgmjhb j

---
+++ src/main/java/nare123/meenu123/App.java
@@ -0,0 +1,13 @@
+package nare123.meenu123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
